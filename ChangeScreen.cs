80b3530d0bba2b9636a027b7cabb4290b7e88c75
Chaohan <32851122+mikeig@users.noreply.github.com>
2018-06-23 11:01:57 -0400

    Simple script for changing screen
    Click to change to another screen(object) and disable the former one

---
+++ ChangeScreen.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ChangeScreen : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject nextScreen, thisScreen;
+    public bool buttonOrNot;
+
+    // private bool isClicked;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!thisScreen)
+        {
+            thisScreen = gameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!buttonOrNot)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (thisScreen.activeInHierarchy)
+                {
+                    thisScreen.SetActive(false);
+                }
+
+                if (!nextScreen.activeInHierarchy)
+                {
+                    nextScreen.SetActive(true);
+                }
+            }
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (thisScreen.activeInHierarchy)
+        {
+            thisScreen.SetActive(false);
+        }
+
+        if (!nextScreen.activeInHierarchy)
+        {
+            nextScreen.SetActive(true);
+        }
+    }
+}
