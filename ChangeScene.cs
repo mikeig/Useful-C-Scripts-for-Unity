d8b0ade657bfd00c64c2bbe72957d8953020f241
Chaohan <32851122+mikeig@users.noreply.github.com>
2018-06-23 10:59:57 -0400

    Simple scene changer
    Change the Unity scene by clicking button.

---
+++ ChangeScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class ChangeScene : MonoBehaviour, IPointerClickHandler
+{
+
+    public int sceneNum;
+
+    
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (Input.GetMouseButtonDown(0))
+  //      {
+            
+  //      }
+	}
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SceneManager.LoadScene(sceneNum);
+    }
+}
