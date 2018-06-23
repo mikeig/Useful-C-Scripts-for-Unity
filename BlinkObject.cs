ad04d66a280d623c83e1db01707af1d0f4ec503d
Chaohan <32851122+mikeig@users.noreply.github.com>
2018-06-23 10:57:56 -0400

    A script for Unity objects to blink
    Blink action based on the time which can be customized.

---
+++ BlinkObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkObject : MonoBehaviour {
+    public GameObject objToBlink;
+
+    private float timer;
+	// Use this for initialization
+	void Start () {
+        timer = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (timer >= 1)
+        {
+            if (objToBlink.activeInHierarchy)
+            {
+                objToBlink.SetActive(false);
+            }
+            else
+            {
+                objToBlink.SetActive(true);
+            }
+            timer -= 1;
+        }
+	}
+}
